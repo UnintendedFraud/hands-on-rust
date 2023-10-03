0c7bb0a1277d326d5bb8baa911231a6f417ff51d
unintendedfraud <briceunintendedfraud@fastmail.com>
2023-10-03 17:06:41 +0200

    blabla

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,11 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 fn main() {
+    let my_list = ["one", "two", "three"];
+
+    for item in my_list {
+        print!("{}", item)
+    }
+
     println!("Hello, world!");
 }
